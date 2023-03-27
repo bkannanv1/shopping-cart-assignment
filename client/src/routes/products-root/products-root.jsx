9ddd7d60c3c423ed18812710c09b98ad5777233d
bkannanv <bharathikannan.v@publicissapient.com>
2023-03-27 13:09:17 +0530

    - Accordian Loaded On Demand.

--- client/src/routes/products-root/products-root.jsx
+++ client/src/routes/products-root/products-root.jsx
@@ -4,8 +4,16 @@ import { Outlet, useLoaderData, useParams } from "react-router-dom";
 import { getProductsAndCategories } from "../../api/endpoints";
 import { getAvailableCategories } from "../utils";
 import { NavLink } from "../../components/nav-link";
-import { Accordian } from "../../components/accordian";
 import { useMediaQuery } from "../../hooks/use-media-query";
+
+const Accordian = React.lazy(() =>
+  /**
+   * Dynamically import a named export
+   */
+  import("../../components/accordian").then((module) => ({
+    default: module.Accordian,
+  }))
+);
 
 export async function loader() {
   const { products, categories } = await getProductsAndCategories();
@@ -42,9 +50,11 @@ export function ProductsRoot() {
       {isInTabletAndAbove ? (
         <CategoryLinks categories={categories} />
       ) : (
-        <Accordian triggerText={pathText}>
-          <CategoryLinks categories={categories} />
-        </Accordian>
+        <React.Suspense fallback={<span>Loading ...</span>}>
+          <Accordian triggerText={pathText}>
+            <CategoryLinks categories={categories} />
+          </Accordian>
+        </React.Suspense>
       )}
       <div className={styles.productsWrapper}>
         <Outlet />
